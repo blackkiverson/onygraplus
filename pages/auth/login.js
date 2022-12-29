ee0afce25a63a36a2c8d72dd21d40e40bed789d5
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-12-29 20:01:17 +0100

    login page (font & Logo)

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,9 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Image from 'next/image';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { IoMail } from 'react-icons/io5';
+import { CgKey } from 'react-icons/cg';
 
 export default function login() {
   return (
-    <main className=" flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900">
+    <main className=" text-center flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900">
       <Image
         src="/bckground.jpg"
         alt=""
@@ -11,18 +15,56 @@ export default function login() {
         priority
         className=" object-cover"
       />
-      <div className="h-full px-5 py-10 bg-gray-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border-gray-100">
-        <form className=' flex-col'>
-          <input type="text" className='px-3 py-0.5 rounded-lg my-4'/>
-          <br/>
-          <input type="text" className='px-3 py-0.5 rounded-lg my-4'/>
-          <br/>
-          <input type="text" className='px-3 py-0.5 rounded-lg my-4'/>
+      <div className=" h-full px-8 py-6 bg-gray-800 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border-gray-100">
+        <section>
+          <img
+            src='/logo.png'
+            alt=''
+          />
+          <div className=" relative flex items-center">
+            <BsFillPersonFill
+              color="white"
+              className=" absolute pointer-events-none mx-2"
+            />
+            <input
+              type="text"
+              className="px-4 py-0.5 rounded-xl bg-bluegray pl-8 text-white"
+            />
+          </div>
           <br />
-          <a href=''><button className=' text-sm font-light text-white my-10'>Forgot Password?</button></a>
-          <br/>
-          <button type="submit" className='bg-blue-800 py-1 px-3 rounded-lg my-2 mx-12 text-md text-white font-semibold'>LOG IN</button>
-        </form>
+          <div className=" relative flex items-center">
+            <IoMail
+              color="white"
+              className=" absolute pointer-events-none mx-2"
+            />
+            <input
+              type="text"
+              className="px-4 py-0.5 rounded-xl bg-bluegray pl-8 text-white"
+            />
+          </div>
+          <br />
+          <div className=" relative flex items-center">
+            <CgKey className=" absolute pointer-events-none mx-2 bg-white rounded" />
+            <input
+              type="password"
+              className="px-4 py-0.5 rounded-xl bg-bluegray pl-8 text-white"
+            />
+          </div>
+          <br />
+          <a href="">
+            <button className=" text-xs font-light text-white my-7 font-Inter">
+              Forgot Password?
+            </button>
+          </a>
+          <br />
+          <button
+            type="submit"
+            autoCorrect="False"
+            className="bg-blue-600 py-1.5 px-5 rounded-2xl my-2 mx-12 text-sm text-white font-medium font-Inter"
+          >
+            LOGIN
+          </button>
+        </section>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bluegray: "#242B2E",
+      },
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif']
+      },
+    }, 
   },
   plugins: [],
 };
